819390591e7083062d244cda8836e1ca63aa0278
Eliud <58745656+eliudio@users.noreply.github.com>
2021-06-17 12:05:05 +0100

    No more warnings during run

--- lib/model/policy_presentation_dropdown_button.dart
+++ lib/model/policy_presentation_dropdown_button.dart
@@ -66,8 +66,8 @@ class PolicyPresentationDropdownButtonWidgetState extends State<PolicyPresentati
 
 List<Widget> widgets(PolicyPresentationModel pm) {
 var widgets = <Widget>[];
-if (pm.documentID != null) widgets.add(new Text(pm.documentID!));
-if (pm.description != null) widgets.add(new Text(pm.description!));
+if (pm.documentID! != null) widgets.add(new Text(pm.documentID!));
+if (pm.description! != null) widgets.add(new Text(pm.description!));
 return widgets;
 }
 
