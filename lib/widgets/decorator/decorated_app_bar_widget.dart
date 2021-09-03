5c339374e5ba7dce82a40ab66528754d057b685e
Eliud <58745656+eliudio@users.noreply.github.com>
2021-09-04 00:10:22 +0100

    Style decor

--- lib/widgets/decorator/decorated_app_bar_widget.dart
+++ lib/widgets/decorator/decorated_app_bar_widget.dart
@@ -11,6 +11,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 /*
+ * The DecoratedAppBarWidget can be used to extend the app bar. It's used for example within eliud_decor_*
+ * packages to add an extra icon.
+ *
  * The DecoratedAppBarWidget adds an icon to the app bar in case we're logged in as the owner of the app.
  * This icon allows to switch on / off the creator mode. The isCreationMode ValueNotifier is/should be used
  * to propagate this switch to other widgets
@@ -20,10 +23,19 @@ class DecoratedAppBarWidget extends StatefulWidget {
   final deco.CreateWidget createOriginalAppBar;
   final String label;
   AppBarModel model;
+  final IconModel lockedIconModel;
+  final IconModel unlockedIconModel;
   final Key? appBarKey;
 
   DecoratedAppBarWidget({
-    Key? key, required this.label, required this.isCreationMode, required this.appBarKey, required this.model, required this.createOriginalAppBar,
+    Key? key,
+    required this.label,
+    required this.isCreationMode,
+    required this.appBarKey,
+    required this.model,
+    required this.createOriginalAppBar,
+    required this.lockedIconModel,
+    required this.unlockedIconModel,
   }) : super(key: key);
 
   @override
@@ -48,9 +60,7 @@ class _DecoratedAppBarWidgetState extends State<DecoratedAppBarWidget> {
         documentID: 'lock_' + widget.label,
         text: 'Lock ' + widget.label,
         description: '',
-        icon: IconModel(
-            codePoint: Icons.lock.codePoint,
-            fontFamily: Icons.lock.fontFamily),
+        icon: widget.lockedIconModel,
         action: FunctionToRun(widget.model.appId, actionToRun: () {
           setState(() {
             widget.isCreationMode.value = false;
@@ -63,9 +73,7 @@ class _DecoratedAppBarWidgetState extends State<DecoratedAppBarWidget> {
         documentID: 'lock_open_' + widget.label,
         text: 'Lock Open ' + widget.label,
         description: '',
-        icon:IconModel(
-            codePoint: Icons.lock_open.codePoint,
-            fontFamily: Icons.lock_open.fontFamily),
+        icon: widget.unlockedIconModel,
         action: FunctionToRun(widget.model.appId, actionToRun: () {
           setState(() {
             widget.isCreationMode.value = true;
@@ -86,15 +94,15 @@ class _DecoratedAppBarWidgetState extends State<DecoratedAppBarWidget> {
 
     // add the right item
     return ValueListenableBuilder(
-      valueListenable: widget.isCreationMode,
-      builder: (context, value, child) {
-        if ((value != null) && (value as bool)) {
-          widget.model.iconMenu!.menuItems!.add(menuItemModelLocked);
-          return widget.createOriginalAppBar();
-        } else {
-          widget.model.iconMenu!.menuItems!.add(menuItemModelUnlocked!);
-          return widget.createOriginalAppBar();
-        }
-      });
+        valueListenable: widget.isCreationMode,
+        builder: (context, value, child) {
+          if ((value != null) && (value as bool)) {
+            widget.model.iconMenu!.menuItems!.add(menuItemModelLocked);
+            return widget.createOriginalAppBar();
+          } else {
+            widget.model.iconMenu!.menuItems!.add(menuItemModelUnlocked!);
+            return widget.createOriginalAppBar();
+          }
+        });
   }
 }
