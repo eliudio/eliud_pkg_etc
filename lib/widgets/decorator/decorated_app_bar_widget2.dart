8f4e4a8ff99298982e0a3198110efef4bdad3a4d
Eliud <58745656+eliudio@users.noreply.github.com>
2021-09-04 09:44:03 +0100

    Improve style decor

---
+++ lib/widgets/decorator/decorated_app_bar_widget2.dart
@@ -0,0 +1,75 @@
+import 'package:eliud_core/decoration/decoration.dart' as deco;
+import 'package:eliud_core/model/app_bar_model.dart';
+import 'package:eliud_core/model/background_model.dart';
+import 'package:eliud_core/model/decoration_color_model.dart';
+import 'package:eliud_core/model/icon_model.dart';
+import 'package:eliud_core/model/menu_item_model.dart';
+import 'package:eliud_core/style/tools/colors.dart';
+import 'package:eliud_core/tools/action/action_model.dart';
+import 'package:eliud_pkg_etc/widgets/decorator/creator_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+/*
+ * The DecoratedAppBarWidget2 can be used to extend the app bar. It's used for example within eliud_decor_*
+ * packages to add an extra icon.
+ *
+ * The DecoratedAppBarWidget2 adds an icon to the app bar in case we're logged in as the owner of the app.
+ * This icon allows to run an action
+ */
+class DecoratedAppBarWidget2 extends StatefulWidget {
+  final deco.CreateWidget createOriginalAppBar;
+  final String label;
+  AppBarModel model;
+  final IconModel iconModel;
+  final Key? appBarKey;
+  final VoidCallback action;
+
+  DecoratedAppBarWidget2({
+    Key? key,
+    required this.label,
+    required this.appBarKey,
+    required this.model,
+    required this.createOriginalAppBar,
+    required this.iconModel,
+    required this.action,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DecoratedAppBarWidgetState2();
+  }
+}
+
+class _DecoratedAppBarWidgetState2 extends State<DecoratedAppBarWidget2> {
+  late MenuItemModel itemModelLocked;
+
+  @override
+  void initState() {
+    super.initState();
+    itemModelLocked = getMenuItemModelLocked();
+  }
+
+  MenuItemModel getMenuItemModelLocked() {
+    return MenuItemModel(
+        documentID: 'lock_' + widget.label,
+        text: 'Lock ' + widget.label,
+        description: '',
+        icon: widget.iconModel,
+        action: FunctionToRun(widget.model.appId, actionToRun: () {
+          widget.action();
+        }));
+  }
+
+  void _delete(MenuItemModel deleteItem) {
+    var items = widget.model.iconMenu!.menuItems!;
+    items.removeWhere((element) => element.documentID == deleteItem.documentID);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _delete(itemModelLocked);
+    widget.model.iconMenu!.menuItems!.add(itemModelLocked);
+    return widget.createOriginalAppBar();
+  }
+}
